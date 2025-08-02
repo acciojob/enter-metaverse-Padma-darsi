87ebcef33c9318cb6714e4e68a6ff3ed39693377
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-08-02 14:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn=document.getElementById("enterBtn");
+btn.addEventListener("click",()=>
+	{
+		const msg=document.getElementById("status");
+		msg.innerHTML="<h1>Entered Metaverse</h1>";
+	});
